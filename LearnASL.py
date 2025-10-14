752b378d7a7995a10a6e32c0201e1245cc26e6bb
Carlisle Poe <carlislepoe@gmail.com>
2025-10-14 14:39:14 -0700

    Added subcategories Letters and Numbers to category/tab Lessons. Also removed placeholder content in Overview tab

--- LearnASL.py
+++ LearnASL.py
@@ -4,27 +4,39 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 
-class GridWindow(Gtk.Window):
+class MainWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="Grid Example")
-        self.set_default_size(400,300)
+        self.set_default_size(500,400)
 
         main_box = Gtk.Box()
-
+        main_box.set_border_width(10)
         self.notebook = Gtk.Notebook()
         self.notebook.set_tab_pos(Gtk.PositionType.TOP)
 
         page1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         page1.set_border_width(10)
-        page1_label = Gtk.Label(label="Lesson tab content")
-        page1.add(page1_label)
+        
+
+        numbers_page = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        numbers_page.set_border_width(10)
+
+
+        letters_page = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        letters_page.set_border_width(10)
+
+
+        nested_notebook = Gtk.Notebook()
+        nested_notebook.set_tab_pos(Gtk.PositionType.TOP)
+        nested_notebook.append_page(letters_page, Gtk.Label(label="Letters"))
+        nested_notebook.append_page(numbers_page, Gtk.Label(label="Numbers"))
+
+        page1.add(nested_notebook)
+
 
         page2 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         page2.set_border_width(10)
-        page2_label = Gtk.Label(label="Overview tab content")
-        page2_button = Gtk.Button(label="Temporary button")
-        page2.add(page2_label)
-        page2.add(page2_button)
+
 
         self.notebook.append_page(page1, Gtk.Label(label="Lessons"))
         self.notebook.append_page(page2, Gtk.Label(label="Overview"))
@@ -33,8 +45,10 @@ class GridWindow(Gtk.Window):
 
         self.add(main_box)
 
+    
 
-win = GridWindow()
+
+win = MainWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
