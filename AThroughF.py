6f47b6825ab164dc467a97f34981657e97c29408
Carlisle Poe <carlislepoe@gmail.com>
2025-10-15 21:29:08 -0700

    Finished the A Through F window.

--- AThroughF.py
+++ AThroughF.py
@@ -187,4 +187,102 @@ class A_Through_F_Window_D(Gtk.Window):
         self.destroy()
 
     def on_next_clicked(self, widget):
-        self.destroy()    
+        self.destroy()
+        a_through_f_window_e = A_Through_F_Window_E()
+        a_through_f_window_e.show_all()
+
+###########################################################################
+
+class A_Through_F_Window_E(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="A Through F")
+        self.set_default_size(300, 300)
+
+        box_main = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing= 6)
+        box_main.set_border_width(10)
+        box_buttons = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing= 6)
+        box_buttons.set_border_width(10)
+
+        image = Gtk.Image()
+        image.set_from_file("assets/E.png")
+        label = Gtk.Label(label="Create a fist, with your thumb folded in over your other four fingers.")
+        label.set_line_wrap(True)
+        label.set_line_wrap_mode(Gtk.WrapMode.WORD)
+        button_prev = Gtk.Button(label = "Previous")
+        button_prev.connect("clicked", self.on_prev_clicked)
+        button_prev.set_size_request(80, 40)
+        button_close = Gtk.Button(label = "Close")
+        button_close.connect("clicked", self.on_close_clicked)
+        button_close.set_size_request(80, 40)
+        button_next = Gtk.Button(label = "Next")
+        button_next.connect("clicked", self.on_next_clicked)
+        button_next.set_size_request(80, 40)
+
+        box_main.pack_start(image, True, True, 0)
+        box_main.pack_start(label, True, True, 0)
+        box_main.pack_start(box_buttons, False, False, 0)
+        box_buttons.pack_start(button_prev, True, True, 0)
+        box_buttons.pack_start(button_close, True, True, 0)
+        box_buttons.pack_start(button_next, True, True, 0)
+        self.add(box_main)
+
+    def on_prev_clicked(self, widget):
+        self.destroy()
+        a_through_f_window_d = A_Through_F_Window_D()
+        a_through_f_window_d.show_all()
+
+    def on_close_clicked(self, widget):
+        self.destroy()
+
+    def on_next_clicked(self, widget):
+        self.destroy()
+        a_through_f_window_f = A_Through_F_Window_F()
+        a_through_f_window_f.show_all()
+
+###########################################################################
+
+class A_Through_F_Window_F(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="A Through F")
+        self.set_default_size(300, 300)
+
+        box_main = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing= 6)
+        box_main.set_border_width(10)
+        box_buttons = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing= 6)
+        box_buttons.set_border_width(10)
+
+        image = Gtk.Image()
+        image.set_from_file("assets/F.png")
+        label = Gtk.Label(label="Point your middle, ring and pinky fingers straight up, and put your thumb and index fingers together to create an O.")
+        label.set_line_wrap(True)
+        label.set_line_wrap_mode(Gtk.WrapMode.WORD)
+        button_prev = Gtk.Button(label = "Previous")
+        button_prev.connect("clicked", self.on_prev_clicked)
+        button_prev.set_size_request(80, 40)
+        button_close = Gtk.Button(label = "Close")
+        button_close.connect("clicked", self.on_close_clicked)
+        button_close.set_size_request(80, 40)
+        button_next = Gtk.Button(label = "Repeat")
+        button_next.connect("clicked", self.on_next_clicked)
+        button_next.set_size_request(80, 40)
+
+        box_main.pack_start(image, True, True, 0)
+        box_main.pack_start(label, True, True, 0)
+        box_main.pack_start(box_buttons, False, False, 0)
+        box_buttons.pack_start(button_prev, True, True, 0)
+        box_buttons.pack_start(button_close, True, True, 0)
+        box_buttons.pack_start(button_next, True, True, 0)
+        self.add(box_main)
+
+    def on_prev_clicked(self, widget):
+        self.destroy()
+        a_through_f_window_e = A_Through_F_Window_E()
+        a_through_f_window_e.show_all()
+
+    def on_close_clicked(self, widget):
+        self.destroy()
+
+    def on_next_clicked(self, widget):
+        self.destroy()
+        a_through_f_window_a = A_Through_F_Window_A()
+        a_through_f_window_a.show_all()
